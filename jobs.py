e273a093f78b3480b8bf19c9a21596ae5f274830
Leo Kirchner <leo.kirchner@networktocode.com>
2024-12-19 08:32:35 +0100

    update

---
+++ jobs.py
@@ -0,0 +1,19 @@
+import csv
+
+from nautobot.apps.jobs import register_jobs, Job, TextVar
+
+
+
+class LocationJob(Job):
+    class Meta:
+        name = "Create Locations from CSV"
+
+    csv_file = TextVar(description="CSV input file.")
+
+    def run(self, csv_file):
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            self.logger.info(row)
+
+
+register_jobs(LocationJob)
